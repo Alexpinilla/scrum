56785431216754903012ee0e49716f4e321578c6
unknown <Alex Pinilla>
2018-10-31 21:48:27 +0100

    Validación de titulos

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
 $(document).ready(function () {
     let addListInput = $('.addListWrapper input');
-    const generateId = namespace => `${namespace}-${Date.now()}-${Math.ceil(Math.random()*100)}`
+    const generateId = namespace => `${namespace}-${Date.now()}-${Math.ceil(Math.random() * 100)}`
     const createListString = name =>
-        `<div class="list contenedor1" id="${generateId('list')}">
-            <div class="listHeader">
-                <h4> · ${name} · <button class="destruir">X</button></h4>
-            </div>
-
-            <div class="addTask">
-                     <ul class="nuevaLista2">
-                        <li class="anadirLista">
-                            <input type="text" class="subTarea"></input>
-                            <button><img src="css/images/mas.png"></button>
-                        </li>
-                        <!--
-                        <ul class="listaDeTareas">
-                        codigo de las tareas
-                        </ul>
-                        <div class="botonAnadirTarea">
-                            <input type="text" class="subTarea"></input>
-                            <button><img src="css/images/mas.png"></button>
-                        </div>
-                        -->
-
-                </ul>
-            </div>
+        `    
+    <div class="list contenedor1" id="${generateId('list')}">
+        <div class="listHeader">
+            <h4> · ${name} · <button class="destruir">X</button></h4>
         </div>
+        <div class="addTask">
+            <ul class="nuevaLista2">
+                <li class="anadirLista">
+                    <input type="text" class="subTarea"></input>
+                    <button onclick="nuevaTarea()"><img src="css/images/mas.png"></button>
+                </li>
+            </ul>
+        </div>
+    </div>
         `
 
     var nuevaSubLista = parametro =>
-        `<li class="list2"><span contenteditable=true class="titulo">${parametro}</span>
+        `
+        <li class="list2"><span contenteditable=true class="titulo">${parametro}</span>
 
         <button class="eli"><img src="css/images/menos.png"></button>
         <button class="editar"><img src="css/images/editar.png"></button> 
@@ -39,22 +30,27 @@ $(document).ready(function () {
             <p>Tarea Completada</p>
         </div>
 
-        </li>`
+        </li>
+        `
 
 
 
     const appendNewList = () => {
         //  cogemos el text del input
         let listName = addListInput.val();
+        if (listName != '') {
+            // creamos el nodo .list
+            let list = $(createListString(listName));
 
-        // creamos el nodo .list
-        let list = $(createListString(listName));
+            // añadimos el node al DOM
+            $('.lists').append(list)
 
-        // añadimos el node al DOM
-        $('.lists').append(list)
-
-        // Limpiamos el texto del input
-        addListInput.val('');
+            // Limpiamos el texto del input
+            addListInput.val('');
+        }
+        else {
+            alert('Introduce un título')
+        }
     }
 
 
@@ -65,53 +61,58 @@ $(document).ready(function () {
         }
     })
 
-    var inputSubTarea = $('.subTarea');
-    // INTRO PARA CREAR TAREA
-    $('.lists').on('keyup', '.list .addTask input', function (intro) {
-        if (intro.keyCode === 13) {
-            var inputSubTarea = $(this);
-            var idcontenedor = $(this.parentNode.parentNode);
-            var tituloSubTarea = inputSubTarea.val();
-            var list2 = $(nuevaSubLista(tituloSubTarea));
-            $(idcontenedor).prepend(list2);
-            inputSubTarea.val('');
-            console.log('hola');
-        }
-    })
-    // ELIMINAR TAREA
-    $('.lists').on('click', '.eli', function (event) {
-        let tarea = $(event.target.parentNode.parentNode);
-        tarea.detach();
-        console.log('eliminando');
-        console.log(event.target.parentNode.parentNode);
-    })
-    // EDITAR TAREA
-    $('.lists').on('click', '.editar', function (event) {
-        let tarea = $(event.target.parentNode);
-        /* tarea.detach(); */
-        $('.titulo').select();
-        console.log(event.target.parentNode.parentNode > span);
-    })
 
-    // Eliminar
-    $('.lists').on('click', '.listHeader button', function (event) {
-        let listNode = $(event.target.parentNode.parentNode.parentNode);
-        listNode.detach();
-    })
- 
-    // Tarea marcada
-    $('.lists').on('click', '.primeraClase', function (event) {
-        $(this).removeClass('cuadrado');
-        $(this).children().removeClass("cuadrado");
-        $(this).addClass('segundaClase');
-        $(this).removeClass('primeraClase');
+        var inputSubTarea = $('.subTarea');
+        // INTRO PARA CREAR TAREA
+        $('.lists').on('keyup', '.list .addTask input', function nuevaTarea(intro) {
+            if (intro.keyCode === 13) {
+                var inputSubTarea = $(this);
+                var idcontenedor = $(this.parentNode.parentNode.parentNode);
+                var tituloSubTarea = inputSubTarea.val();
+                if (tituloSubTarea != '') {
+                    var list2 = $(nuevaSubLista(tituloSubTarea));
+                    $(idcontenedor).prepend(list2);
+                    inputSubTarea.val('');
+                }
+                else {
+                    alert('Introduce in título')
+                }
+            }
+        })
+        // ELIMINAR TAREA
+        $('.lists').on('click', '.eli', function (event) {
+            let tarea = $(event.target.parentNode.parentNode);
+            tarea.detach();
+            console.log('eliminando');
+            console.log(event.target.parentNode.parentNode);
+        })
+        // EDITAR TAREA
+        $('.lists').on('click', '.editar', function (event) {
+            let tarea = $(event.target.parentNode);
+            /* tarea.detach(); */
+            $('.titulo').select();
+            console.log(event.target.parentNode.parentNode > span);
+        })
+
+        // Eliminar
+        $('.lists').on('click', '.listHeader button', function (event) {
+            let listNode = $(event.target.parentNode.parentNode.parentNode);
+            listNode.detach();
+        })
+
+        // Tarea marcada
+        $('.lists').on('click', '.primeraClase', function (event) {
+            $(this).removeClass('cuadrado');
+            $(this).children().removeClass("cuadrado");
+            $(this).addClass('segundaClase');
+            $(this).removeClass('primeraClase');
+
+        })
+        $('.lists').on('click', '.segundaClase', function (event) {
+            console.log(this);
+            $(this).children(':first').addClass("cuadrado");
+            $(this).removeClass('segundaClase');
+            $(this).addClass('primeraClase');
+        })
 
     })
-    $('.lists').on('click', '.segundaClase', function (event) {
-        console.log(this);
-        $(this).children(':first').addClass("cuadrado");
-        $(this).removeClass('segundaClase');
-        $(this).addClass('primeraClase');
-    })
-
-})
